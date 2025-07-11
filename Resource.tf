90776ae35c45e0e6ed1767198311722404b937a7
Santan Singh Rawat <Santan@upsy0910gmail.onmicrosoft.com>
2025-07-11 10:39:55 +0000

    This is test3

--- Resource.tf
+++ Resource.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "Resource_group_1" {
     name = "B17_test1_Resource_group"
     location = "east us"
 }
+resource "azurerm_resource_group" "Resource_group_3" {
+    name = "B17_test3_Resource_group"
+    location = "east us"
+}
